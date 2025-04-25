2f311d6ed80b2a14b6b3fdda6d47d93e515283e5
safatalnur <safatalnur@gmail.com>
2025-04-25 01:43:02 -0400

    FEAT: add header, body, and footer section

---
+++ footer.php
@@ -0,0 +1,6 @@
+        <footer>
+            <p>This is the footer section</p>
+        </footer>
+
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php get_header(); ?>
+
+    <h1>This is the body section</h1>
+
+<?php get_footer(); ?>
