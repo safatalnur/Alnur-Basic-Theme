b9a9ec2df33919110ad22a128cfe4de7bf153ccd
safatalnur <safatalnur@gmail.com>
2025-04-27 01:23:56 -0400

    STYLE: remove test navbar and add some basic styling

--- header.php
+++ header.php
@@ -7,23 +7,31 @@
     <?php wp_head(); ?>
 </head>
 <body>
+    <!-- Bootstrap 5.2.3 navbar -->
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-            <a class="nav-link" href="#">Features</a>
-            <a class="nav-link" href="#">Pricing</a>
-            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <!-- <a class="nav-link active" aria-current="page" href="#">Home</a>
+                <a class="nav-link" href="#">Features</a>
+                <a class="nav-link" href="#">Pricing</a>
+                <a class="nav-link disabled" aria-disabled="true">Disabled</a> -->
+                <?php wp_nav_menu(array(
+                        'theme_location'    => 'primary',
+                        'container'         => 'nav',
+                        'container_class'   => 'primary-nav',
+                        'menu_class'        => 'nav-menu',
+                        'menu_id'           => 'main-menu',
+                        'link_before'       => '<span class="menu-icon"></span>',
+                        'link_after'        => ''
+                    ));
+                ?>
+            </div>
+            </div>
         </div>
-        </div>
-    </div>
     </nav>
-    <?php wp_nav_menu(array(
-        'theme_location'  => 'primary'
-    ));?>
     <p>This is the heading section<i class="fa-solid fa-user"></i></p>
